3b84cc234e7c4261cf9e8e6317d5d2769a4ef452
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-03 15:09:36 -0700

    Fixed story resizing

--- 209politics/js/index.js
+++ 209politics/js/index.js
@@ -1,6 +1,16 @@
+function resize(){
+  var c = document.getElementsByClassName("storycontainer");
+  var t = document.getElementsByClassName("storytextcontainer");
+  var i = document.getElementsByClassName("storyimgcontainer");
+  for(var j = 0; j < c.length; j++){
+    c[j].style.height = (Math.max(t[j].offsetHeight, i[j].offsetHeight)+20)+'px';
+  }
+}
+
 window.onload = function(){
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var d = new Date();
   document.getElementById("date").innerHTML = days[d.getDay()]+", "+months[d.getMonth()]+" "+d.getDate().toString()+", "+d.getFullYear().toString();
+  resize();
 };
