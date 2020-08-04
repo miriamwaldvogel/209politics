69806cc9c986960ce45e51b7c799be674df7e08f
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-03 20:38:11 -0700

    Moved resize();

--- 209politics/js/index.js
+++ 209politics/js/index.js
@@ -8,9 +8,9 @@ function resize(){
 }
 
 window.onload = function(){
+  resize();
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   var d = new Date();
   document.getElementById("date").innerHTML = days[d.getDay()]+", "+months[d.getMonth()]+" "+d.getDate().toString()+", "+d.getFullYear().toString();
-  resize();
 };
