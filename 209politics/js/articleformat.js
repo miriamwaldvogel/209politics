dac7419d4fc4d63f5504290483d288786c2e579c
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-04 21:23:37 -0700

    Created generic article format js

---
+++ 209politics/js/articleformat.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+  var c = document.getElementsByClassName("storycontainer");
+  var t = document.getElementsByClassName("storytextcontainer");
+  var i = document.getElementsByClassName("storyimgcontainer");
+  for(var j = 0; j < c.length; j++){
+    c[j].style.height = (Math.max(t[j].offsetHeight, i[j].offsetHeight)+20)+'px';
+  }
+};
