a839ec41b24511f0937f13b96502623b588f6769
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-04 18:06:44 -0700

    Added header

---
+++ 209politics/js/article.js
@@ -0,0 +1,8 @@
+function sidebar(){
+  b = document.getElementById("sectionscontainer");
+  if (b.style.display === "block"){
+    b.style.display = "none";
+  }else{
+    b.style.display = "block";
+  }
+}
