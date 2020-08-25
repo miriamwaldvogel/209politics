a6433630a03e1426fc3bddbbbcdff9ce7c79ac59
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-24 20:16:23 -0700

    tracy special election

---
+++ 209politics/2020/08/25/tracy-special-election.js
@@ -0,0 +1,25 @@
+function votes(){
+  var y = document.getElementById("yes");
+  var n = document.getElementById("no");
+  var yv = parseInt(y.innerHTML);
+  var nv = parseInt(n.innerHTML);
+  var t = yv+nv;
+  var w = document.getElementById("main").offsetWidth - Math.max(y.offsetWidth, n.offsetWidth);
+  document.getElementById("yescontainer").style.width = w*(yv/t)+'px';
+  document.getElementById("nocontainer").style.width = w*(nv/t)+'px';
+}
+window.onload = function(){
+  var y = document.getElementById("yes");
+  var n = document.getElementById("no");
+  var yv = parseInt(y.innerHTML);
+  var nv = parseInt(n.innerHTML);
+  var t = yv+nv;
+  if(t>0){
+    y.innerHTML += " votes - "+ (yv/t).toFixed(4)*100 + '%';
+    n.innerHTML += " votes - "+ (nv/t).toFixed(4)*100 + '%';
+    var w = document.getElementById("main").offsetWidth - Math.max(y.offsetWidth, n.offsetWidth);
+    document.getElementById("yescontainer").style.width = w*(yv/t)+'px';
+    document.getElementById("nocontainer").style.width = w*(nv/t)+'px';
+    document.getElementById("total").innerHTML += t+" votes";
+  }
+};
