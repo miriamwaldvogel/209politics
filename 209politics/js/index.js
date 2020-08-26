a3ebe7f3a3e752961beaa4730feafe0e26060fb6
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-25 21:52:02 -0700

    index redesign

--- 209politics/js/index.js
+++ 209politics/js/index.js
@@ -1,20 +1,25 @@
 function resize(){
-  var c = document.getElementsByClassName("storycontainer");
+  var c = document.getElementsByClassName("bigstory");
   var t = document.getElementsByClassName("storytextcontainer");
   var i = document.getElementsByClassName("storyimgcontainer");
+  var h = 0;
   if (window.innerWidth <= 667){
-    for(var k = 0; k < i.length; k++){
+    for(var k = 0; k < c.length; k++){
       c[k].style.height = (t[k].offsetHeight+i[k].offsetHeight+10)+'px';
     }
   }else{
-    for(var j = 0; j < i.length; j++){
+    for(var j = 0; j < c.length; j++){
       c[j].style.height = (Math.max(t[j].offsetHeight, i[j].offsetHeight)+20)+'px';
+      h += c[j].offsetHeight;
     }
+    document.getElementById("covidcontainer").style.height = h+document.getElementById("mainstorycontainer").offsetHeight+'px';
+    document.getElementById("coviddatacontainer").style.height = h+document.getElementById("mainstorycontainer").offsetHeight-100+'px';
   }
-  var s = document.getElementsByClassName("smallstory");
-  var u = document.getElementsByClassName("smalltext");
-  for(var l = 0; l < s.length; l++){
-    s[l].style.height = (u[l].offsetHeight+20)+'px';
+  var l = document.getElementsByClassName("leftballot");
+  var r = document.getElementsByClassName("rightballot");
+  for(var m = 0; m < l.length; m++){
+    l[m].style.height = Math.max(l[m].offsetHeight, r[m].offsetHeight)+'px';
+    r[m].style.height = Math.max(l[m].offsetHeight, r[m].offsetHeight)+'px';
   }
 }
 window.onload = function(){
