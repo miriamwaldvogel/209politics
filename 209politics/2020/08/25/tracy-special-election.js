3ed35383deee83b3b76d62e32c4d062972396d30
miriamwaldvogel <miriamwaldvogel@penguin>
2020-08-25 21:51:55 -0700

    tracy special election

--- 209politics/2020/08/25/tracy-special-election.js
+++ 209politics/2020/08/25/tracy-special-election.js
@@ -15,8 +15,8 @@ window.onload = function(){
   var nv = parseInt(n.innerHTML);
   var t = yv+nv;
   if(t>0){
-    y.innerHTML += " votes - "+ (yv/t).toFixed(4)*100 + '%';
-    n.innerHTML += " votes - "+ (nv/t).toFixed(4)*100 + '%';
+    y.innerHTML += " votes - "+ ((yv/t)*100).toFixed(2) + '%';
+    n.innerHTML += " votes - "+ ((nv/t)*100).toFixed(2) + '%';
     var w = document.getElementById("main").offsetWidth - Math.max(y.offsetWidth, n.offsetWidth);
     document.getElementById("yescontainer").style.width = w*(yv/t)+'px';
     document.getElementById("nocontainer").style.width = w*(nv/t)+'px';
