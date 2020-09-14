fecd02e519a443ae38df8fbe7ba182052706b533
miriamwaldvogel <miriamwaldvogel@penguin>
2020-09-14 13:43:13 -0700

    search initial

---
+++ 209politics/php/search.php
@@ -0,0 +1,81 @@
+<?php
+$txt = array('Banta Elementary School District May Become K-12 if Approved by Voters' => 'River Islands residents could soon have access to a unified K-12 school district if a ballot
+measure to expand the Banta Elementary School District is approved. The district would
+incorporate a high school in River Islands scheduled to open in 2022 and would be known as
+the Banta Unified School District. Currently, student residents of River Islands attend a Banta
+school for K-8 and then West High School in Tracy.
+The governing board of the new district would comprise of five members. Two would be
+selected by the current Banta Elementary Board and two would be appointed by the River
+Islands homeowners agency once construction is finished. The final, at-large member would be
+appointed by the other four members. River Islands Tech Academy and the Next Generation
+STEAM Academy would be incorporated into the River Islands Charter Schools organization,
+according to the Manteca Bulletin. The charter schools would be directed by Brenda Scholl,
+currently the principal of River Islands Tech Academy.
+Construction is scheduled to begin on a high school in River Islands in 2021, which
+would serve the students of the new district. Once it has opened, Banta Elementary School
+students will continue to have the option to attend Tracy High School. Banta Elementary was not the only district that the Cambay Group, the developer of
+River Islands, explored for unification. The group briefly considered incorporating into the
+Manteca Unified School District back when construction on the development began.
+',
+'Breaking Down San Joaquin County’s Seven November Ballot Proposals' => 'Measure A
+Measure A is a $260 million bond issue for upgrading school facilities, disability accommodations, career training programs, and classrooms for coronavirus precautions in Manteca Unified School District. The measure would add an extra $45 per every $100,000 of a home’s value in property taxes and is estimated to generate around $13.9 million annually. It requires 55% of voter approval.
+This proposal might sound familiar. A similar version was on the ballot back in March, where it proposed a tax of $60 per $100,000 of home value instead of $45. Additionally, there was no need for coronavirus-related updates to classrooms (which the new proposal will cover) at the time. The measure failed, garnering just slightly more than 50% of the vote.
+Measure U
+The Woodbridge Rural Fire Protection District is seeking a property tax at a flat rate of $45 for unimproved parcels and 8 cents per building square foot for improved parcels. Revenues from the tax, estimated annually at about $1.2 million, would go towards maintaining emergency response times, rescue services, firefighting equipment, and emergency medical positions. Like many taxpayer-funded services, the district is expected to see a dip in tax revenue this year due to the coronavirus pandemic.
+Measure V
+Measure V would expand Banta Elementary School District into Banta Unified School District, allowing it to have a high school. Currently, residents of River Islands have to send their kids to West High School in Tracy Unified. The new district’s high school would be River Islands High School, scheduled to open in 2022. Once it is open, Banta Elementary School students will continue to have the option to attend Tracy High.
+Measure W
+Cannabis and hemp businesses in Tracy would be subject to a tax netting approximately $350,000 to $750,000 a year. The rate of taxation would depend on the type of businesses: $12 per square foot for cultivators, 6% for retailers, 2.5% for laboratories, 3% and 4% for all other businesses. Businesses with multiple cannabis licenses would have their taxes capped at 15%. A similar proposal was on the ballot in 2018, but required ⅔ approval to pass rather than this year’s 50%. The projected revenue was also considerably smaller (at $35,000 to $100,000 annually) and was supposed to go to law enforcement rather than the city’s general fund.
+Measure X
+Another cannabis tax is also on the ballot, this one county-wide. Cannabis businesses in unincorporated areas of the county would be taxed at 3.5% of sales, with an additional $2 per square foot for cultivation enterprises. It requires ⅔ assent to pass. Revenues for the tax are difficult to estimate, as there are currently no approved cannabis businesses in unincorporated areas of San Joaquin County, but they would go towards early childhood education, public health services, and homeless mitigation. The exact same measure, back then known as Measure B, was on the ballot two years ago and failed to pass.
+Measure Y
+Measure Y is asking voters to approve an exception to Tracy’s growth management ordinance to allow for development for a potential ValleyLink station. ValleyLink, which is seeking to link the Dublin-Pleasanton BART station and the ACE (Altamont Corridor Express), has proposed a station in downtown Tracy. However, the station must be built in an area with at least 2,200 housing units in a half-mile radius.
+The proposed area for the station currently contains 1,750 housing units. However, construction of new homes is capped at 600 per year by the growth management ordinance. Approving the exception in Measure Y would allow for the area to reach the required 2,200 units much faster. At least 10% of the new homes will be designated as affordable housing.
+Measure Z
+The city of Manteca is asking voters to increase the sales tax by 1%, from 8.25% to 9.25%. The measure would add another $12 million to the city’s general fund to cope with drops in sales, hotel room, and gas taxes as a result of the coronavirus pandemic. The sales tax was last altered in 2017, when it decreased from 8.5% to 8.25% as state and county rates changed. Measure M was also approved back in 2006, which enacted a 0.5% increase that went towards public safety.
+',
+'Cannabis Tax for San Joaquin Unincorporated Areas Will Appear on November Ballot' => 'A potential tax on cannabis businesses in unincorporated areas of San Joaquin County will be decided by voters on November 3rd. Income from the tax will go towards early childhood education, public health services, and homeless mitigation. The amount of revenue it is expected to generate is unsure, as no cannabis licenses have been approved for a business in an unincorporated area.
+	The tax would start at 3.5% of sales and an additional $2 per square foot for cultivation businesses. The San Joaquin Board of Supervisors would be able to raise the sales tax as high as 8%. The rate for cultivation could also be adjusted by the board, but could not exceed the Consumer Price Index for the San Francisco-Oakland-San Jose area. Revenues from the tax would be divided in half. 50% would go to the County Children’s Trust Fund for literacy, drug prevention, after-school, home visitation, and other youth programs for underserved communities. The other half would go towards public health services, homeless mitigation, and enforcing existing cannabis laws.
+	The exact same measure was also on the ballot two years ago, back then known as Measure B. It required ⅔ vote to pass (just as this year’s proposal will), and was not approved. Only 61.5% (58,757 votes) were for yes, with 38.5% (36,726 votes) for no. This year’s ballot initiative was requested by the San Joaquin Children’s Alliance.
+	The measure was approved by the County Board of Supervisors 3-2, with Supervisors Chuck Winn and Bob Elliott dissenting. Supervisor Elliott noted that sending half of the revenues to the Children’s Trust Fund would likely not adequately offset county expenses for cannabis businesses. The county estimated in 2017 that a cannabis tax would generate approximately $4.9 million a year, assuming a “mature market with 13 cultivation businesses and 7 non-cultivation businesses”. The actual tax will accrue far less, as there are currently no cannabis businesses in unincorporated San Joaquin County. County cannabis-related expenses total approximately $4.1 million to $4.6 million a year.',
+'Cannabis Tax in Tracy to be Voted on in November'=>'Among the measures for Tracy voters to consider in this November’s election will be a
+general tax on cannabis and hemp businesses. The proposal is expected to net between
+$350,000 to $750,000 in revenue for the city’s general fund. There are at least 20 cannabis
+businesses in the Tracy area, most of them delivery-only.
+An almost identical proposal, Measure D, was also up for vote in 2018 and required 2⁄3
+approval to pass. It was defeated, with 62.2% (16,187 votes) for yes and 37.8% (9,819 votes)
+for no. Cannabis businesses were still prohibited from operating in Tracy at the time, although
+regulations were being drafted to allow two delivery-only businesses. As a result, the projected
+revenue was considerably smaller than the current proposal at $35,000 to $100,000 a year.
+Additionally, the tax income was supposed to go to police and code enforcement rather than the
+city’s general fund.
+This November’s measure requires only 50% approval to go into effect, making it likely
+to pass. The tax rates would depend on the type of business: $12 per square foot for cultivators,
+6% for retailers, 2.5% for laboratories, 3% for distributors, and 4% for all other businesses.
+These rates would be maximums, with the City Council able to adjust the rates as they see fit.
+According to the Tracy Press, cannabis businesses with multiple licenses would have their tax
+rate capped at 15%.
+');
+$ref = array("Breaking Down San Joaquin County’s Seven November Ballot Proposals"=>"/2020/08/13/san-joaquin-ballot-measures.html", "Cannabis Tax for San Joaquin Unincorporated Areas Will Appear on November Ballot"=>"/2020/08/05/san-joaquin-cannabis-tax.html", "Banta Elementary School District May Become K-12 if Approved by Voters"=>"/2020/08/03/banta-river-islands-district.html", "Cannabis Tax in Tracy to be Voted on in November"=>"/2020/08/02/tracy-cannabis-tax.html");
+$key = explode(" ", "cannabis Joaquin school Banta");
+$f = array();
+foreach($txt as $i => $j){
+  $t = 0;
+  $b = 0;
+  foreach($key as $k){
+    $a = substr_count(strtoupper($j), strtoupper($k))+substr_count(strtoupper($i), strtoupper($k));
+    $t+= $a;
+    if ($a > 0){
+      $b += 1;
+    }
+  }
+  if ($b > 0){
+    $f[$i] = array($b, $t);
+  }
+}
+
+arsort($f);
+foreach($f as $i=>$k){
+  echo "<a href=\"".$ref[$i]."\"><p>".$i."</p>"."</a>"." \n";
+}
+ ?>
