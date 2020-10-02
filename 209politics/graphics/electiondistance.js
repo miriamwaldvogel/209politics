c16b6d30d99234176f871e96a5b934d9f98be287
miriamwaldvogel <miriamwaldvogel@penguin>
2020-10-01 19:21:28 -0700

    old removals

--- 209politics/graphics/electiondistance.js
+++
@@ -1,3 +0,0 @@
-window.onload = function(){
-  document.getElementById("bolded").innerHTML = ' '+Math.floor((new Date("11/03/2020").getTime()- new Date().getTime())/(1000*3600*24))+' days.';
-};
--- 209politics/js/header.js
+++
@@ -1,8 +0,0 @@
-function sidebar(){
-  b = document.getElementById("sectionscontainer");
-  if (b.style.display === "block"){
-    b.style.display = "none";
-  }else{
-    b.style.display = "block";
-  }
-}
