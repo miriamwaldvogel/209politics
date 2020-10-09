728e0a79eb8ed09894ae66a7bd42e9c12741770d
miriamwaldvogel <miriamwaldvogel@penguin>
2020-10-09 13:48:49 -0700

    voter graphics

--- graphics/electiondistance.js
+++ graphics/electiondistance.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-  document.getElementById("bolded").innerHTML = ' '+Math.floor((new Date("11/03/2020").getTime()- new Date().getTime())/(1000*3600*24))+' days.';
+  document.getElementById("bolded").innerHTML = ' '+(Math.floor((new Date("11/03/2020").getTime()- new Date().getTime())/(1000*3600*24))+1)+' days.';
 };
