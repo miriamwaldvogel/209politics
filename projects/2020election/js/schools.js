ae004703fd00a0bd1bbeb177a33c243839cbe5fb
miriamwaldvogel <miriamwaldvogel@penguin>
2020-11-13 07:42:18 -0800

    removed election refresh

--- projects/2020election/js/schools.js
+++ projects/2020election/js/schools.js
@@ -6,20 +6,3 @@ var schools = function(){
     b.style.display = "block";
   }
 };
-window.onload = function(){
-  window.setInterval("reload();", 60000);
-  var c = 60;
-  window.setInterval(function(){
-    document.getElementById("refreshing").innerHTML = "Refreshing in "+c+" seconds";
-    c-=1;
-    if (c < 0){
-      c = 60;
-    }
-  }, 1000)
-};
-var reload = function(){
-    var d = document.getElementsByClassName("results");
-    for (var i = 0; i < d.length; i++){
-      d[i].src = d[i].src;
-    }
-};
