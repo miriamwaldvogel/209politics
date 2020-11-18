cf18d4573413fbafdcd5eec8ee824eb7206e5067
miriamwaldvogel <miriamwaldvogel@penguin>
2020-11-18 09:12:18 -0800

    opinion submit

--- js/opinion.js
+++ js/opinion.js
@@ -1,5 +1,10 @@
 function resizetop(){
-  document.getElementById("topcontainer").style.height = Math.max(document.getElementById("leftpanel").offsetHeight, document.getElementById("rightpanel").offsetHeight)+10+'px';
+  if (window.innerWidth > 620){
+    document.getElementById("topcontainer").style.height = Math.max(document.getElementById("leftpanel").offsetHeight,document.getElementById("rightpanel").offsetHeight)+10+'px';
+  }
+  else{
+    document.getElementById("topcontainer").style.height = document.getElementById("leftpanel").offsetHeight + document.getElementById("rightpanel").offsetHeight+10+'px';
+  }
 }
 window.onload = function(){
   resizetop();
