098ae4563a0ca92b8ae1caba4118b03562c093de
miriamwaldvogel <miriamwaldvogel@penguin>
2020-11-19 08:15:17 -0800

    submit pages

---
+++ opinion/js/submit.js
@@ -0,0 +1,8 @@
+function submitlink(){
+  l = document.getElementById("formcontainer");
+  if (l.style.display === "block"){
+    l.style.display = "none";
+  } else{
+    l.style.display = "block";
+  }
+}
