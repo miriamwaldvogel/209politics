1acd72d32cc9b78f67b82d73e61621841db94efe
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-02 16:18:38 -0800

    template fixes

--- templates/aws.py
+++ templates/aws.py
@@ -8,6 +8,17 @@ latest = IntVar()
 opinion = IntVar()
 policy = IntVar()
 insta = IntVar()
+int = IntVar()
+def aws():
+    a = ["contact", "covid-19", "elections", "latest", "opinion", "policy", "instagram", "interactives"]
+    b = [contact.get(), covid.get(), elections.get(), latest.get(), opinion.get(), policy.get(), insta.get(), int.get()]
+    s = 'aws s3 sync /home/miriamwaldvogel/209politics/ s3://209politics.com --exclude "/home/miriamwaldvogel/209politics/.git/*" --exclude "/home/miriamwaldvogel/209politics/templates/*" --exclude "/home/miriamwaldvogel/209politics/opinion/*";'
+    for i, j in enumerate(b):
+        if j == 1:
+            s+='aws s3 cp /home/miriamwaldvogel/209politics/%s1.html s3://209politics.com/%s;' % (a[i], a[i])
+    system(s)
+    global w
+    w.destroy()
 Checkbutton(w, text="Contact", variable=contact).pack()
 Checkbutton(w, text="COVID", variable=covid).pack()
 Checkbutton(w, text="Elections", variable=elections).pack()
@@ -15,12 +26,6 @@ Checkbutton(w, text="Latest", variable=latest).pack()
 Checkbutton(w, text="Opinion", variable=opinion).pack()
 Checkbutton(w, text="Policy", variable=policy).pack()
 Checkbutton(w, text="Instagram", variable=insta).pack()
-Button(w, text="Submit", command=w.quit).pack()
+Checkbutton(w, text="Interactives", variable=int).pack()
+Button(w, text="Submit", command=aws).pack()
 w.mainloop()
-a = ["contact", "covid-19", "elections", "latest", "opinion", "policy", "instagram"]
-b = [contact.get(), covid.get(), elections.get(), latest.get(), opinion.get(), policy.get(), insta.get()]
-s = 'aws s3 sync /home/miriamwaldvogel/209politics/ s3://209politics.com --exclude "/home/miriamwaldvogel/209politics/.git/*" --exclude "/home/miriamwaldvogel/209politics/templates/*" --exclude "/home/miriamwaldvogel/209politics/opinion/*";'
-for i, j in enumerate(b):
-    if j == 1:
-        s+='aws s3 cp /home/miriamwaldvogel/209politics/%s1.html s3://209politics.com/%s;' % (a[i], a[i])
-system(s)
