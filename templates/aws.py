a9e550d3e07005f990175f2a1e02b9d168712d36
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-09 17:12:02 -0800

    new articles

--- templates/aws.py
+++ templates/aws.py
@@ -10,6 +10,8 @@ policy = IntVar()
 insta = IntVar()
 int = IntVar()
 def aws():
+    global w
+    w.destroy()
     a = ["contact", "covid-19", "elections", "latest", "opinion", "policy", "instagram", "interactives"]
     b = [contact.get(), covid.get(), elections.get(), latest.get(), opinion.get(), policy.get(), insta.get(), int.get()]
     s = 'aws s3 sync /home/miriamwaldvogel/209politics/ s3://209politics.com --exclude "/home/miriamwaldvogel/209politics/.git/*" --exclude "/home/miriamwaldvogel/209politics/templates/*" --exclude "/home/miriamwaldvogel/209politics/opinion/*";'
@@ -17,8 +19,6 @@ def aws():
         if j == 1:
             s+='aws s3 cp /home/miriamwaldvogel/209politics/%s1.html s3://209politics.com/%s;' % (a[i], a[i])
     system(s)
-    global w
-    w.destroy()
 Checkbutton(w, text="Contact", variable=contact).pack()
 Checkbutton(w, text="COVID", variable=covid).pack()
 Checkbutton(w, text="Elections", variable=elections).pack()
