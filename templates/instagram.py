d7df43d154ee7cf7d0acfabf8863eb6812c3beb4
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-12 09:31:12 -0800

    abs path

--- templates/instagram.py
+++ templates/instagram.py
@@ -22,7 +22,7 @@ def instagram():
 
 Label(w, text="HTML name").pack()
 htmlname.pack()
-Label(w, text="Image name").pack()
+Label(w, text="Image path").pack()
 img.pack()
 Label(w, text="Alt").pack()
 alt.pack()
