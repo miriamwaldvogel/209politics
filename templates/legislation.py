f5227d961088dd53e9d416110ecfad4f2d77e6a2
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-18 19:58:18 -0800

    filter

--- templates/legislation.py
+++ templates/legislation.py
@@ -1,18 +1,33 @@
 import csv
-f = open('/home/miriamwaldvogel/209politics/projects/legislationtracker/data/harder-leg.csv', 'r')
+f = open('/home/miriamwaldvogel/209politics/projects/legislativetracker/data/harder-leg.csv', 'r')
 r = csv.reader(f)
 next(r, None)
 s = ''
-for i in r:
+filter = {'role':{
+'sponsor': [],
+'cosponsor': []
+}, 'status':{
+'intro': [],
+'house': [],
+'senate': [],
+'topres': [],
+'law': [],
+'resdif': [],
+'veto': []
+}}
+for j, i in enumerate(r):
     a = ['', '', '', '', '']
-    a[i[7:].index('TRUE')] = " current"
+    legstatus = i[8:].index('TRUE')
+    a[legstatus] = " current"
+    filter['status'][list(filter['status'].keys())[legstatus]].append(j)
     s += """
     <div class="leg show">
-        <p class="name"><a href="/projects/legislationtracker/%s/%s.html" class="leglink">%s - %s</a></p>
+        <p class="name"><a href="/projects/legislativetracker/%s/%s.html" class="leglink">%s - %s</a></p>
         <p class="desc">%s</p>
         <p class="date">Introduced: %s</p>
         <p class="sponsor">Sponsor: %s | <a href="%s" class="cosponsorlink" target="_blank">view cosponsors</a></p>
         <p class="com">Committees: %s</p>
+        <p class="action">Latest action: %s</p>
         <div class="statuscontainer">
             <p class="statustitle">Status: </p>
             <p class="status%s">Introduced</p>
@@ -21,14 +36,18 @@ for i in r:
             <p class="status%s">To President</p>
             <p class="status%s">Became Law</p>
         </div>
-    </div>
-""" % (i[1].lower(), i[0].replace('.', '', 2).lower().replace(' ', '-', 1), i[1], i[0], i[2], i[3], i[4], i[5], i[6], a[0], a[1], a[2], a[3], a[4])
+    </div>""" % (i[1].lower(), i[0].replace('.', '', 2).lower().replace(' ', '-', 1), i[1], i[0], i[2], i[3], i[4], i[5], i[6], i[7], a[0], a[1], a[2], a[3], a[4])
+    if "Harder" in i[4]:
+        filter['role']['sponsor'].append(j)
+    else:
+        filter['role']['cosponsor'].append(j)
 f.close()
-f = open('/home/miriamwaldvogel/209politics/projects/legislationtracker/people/josh-harder.html', 'r')
+f = open('/home/miriamwaldvogel/209politics/projects/legislativetracker/people/josh-harder.html', 'r')
 g = f.read().split('<div id="legislation">')
 f.close()
-f = open('/home/miriamwaldvogel/209politics/projects/legislationtracker/people/josh-harder.html', 'w')
+f = open('/home/miriamwaldvogel/209politics/projects/legislativetracker/people/josh-harder.html', 'w')
 f.write(g[0]+'<div id="legislation">')
 f.write(s)
-f.write(g[1])
+h = g[1].split('<div id="footer">')[1].split('<script type="text/javascript">')
+f.write('</div>\n</div>'+'<div id="footer">'+h[0]+'<script type="text/javascript">\n'+'var para = %s' % filter+h[1])
 f.close()
