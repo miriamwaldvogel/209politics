0776ed04f53077c5600b11bd1527b3c4a1805cc4
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-20 12:16:45 -0800

    exclude leg tracker

--- templates/aws.py
+++ templates/aws.py
@@ -14,7 +14,7 @@ def aws():
     w.destroy()
     a = ["contact", "covid-19", "elections", "latest", "opinion", "policy", "instagram", "interactives"]
     b = [contact.get(), covid.get(), elections.get(), latest.get(), opinion.get(), policy.get(), insta.get(), int.get()]
-    s = 'aws s3 sync /home/miriamwaldvogel/209politics/ s3://209politics.com --exclude "/home/miriamwaldvogel/209politics/.git/*" --exclude "/home/miriamwaldvogel/209politics/templates/*" --exclude "/home/miriamwaldvogel/209politics/opinion/*";'
+    s = 'aws s3 sync /home/miriamwaldvogel/209politics/ s3://209politics.com --exclude "/home/miriamwaldvogel/209politics/.git/*" --exclude "/home/miriamwaldvogel/209politics/templates/*" --exclude "/home/miriamwaldvogel/209politics/opinion/*" --exclude "/home/miriamwaldvogel/209politics/projects/legislativetracker/*";'
     for i, j in enumerate(b):
         if j == 1:
             s+='aws s3 cp /home/miriamwaldvogel/209politics/%s1.html s3://209politics.com/%s;' % (a[i], a[i])
