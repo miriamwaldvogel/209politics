cb7029445b6e6f88af98d57890cf70f668d119b4
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-24 20:29:49 -0800

    rename to crimemap

--- templates/911.py
+++ templates/911.py
@@ -1,3 +1,15 @@
 import csv
 f = open('/home/miriamwaldvogel/209politics/projects/911calls/data/stockton.csv', 'r')
-r = csv.reader()
+r = csv.reader(f)
+next(r)
+s = ''
+for i in r:
+    s += """L.marker([%s]).bindPopup("<p class='popuptitle'>%s</p><p class='info'>%s at %s - %s</p><p class='details' onclick='show(event);'>Details</p><p class='desc' style='display: none;'>%s</p><p class='hide' style='display: none;' onclick='hide(event);'>Hide</p>").addTo(markers);\n"""%(i[4], i[0], i[1], i[2].lower(), i[3], i[5])
+f.close()
+l = "var markers = L.layerGroup();"
+f = open('/home/miriamwaldvogel/209politics/projects/911calls/stockton.html', 'r')
+g = f.read().split(l, 1)
+f.close()
+f = open('/home/miriamwaldvogel/209politics/projects/911calls/stockton.html', 'w')
+f.write(g[0]+l+"\n"+s+g[1].split("var map", 1)[1]+"var map")
+f.close()
