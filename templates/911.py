342c5aa76ead33311f475dbb8c23807492161547
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-25 20:22:21 -0800

    marker accuracy

--- templates/911.py
+++ templates/911.py
@@ -1,5 +1,5 @@
 import csv
-f = open('/home/miriamwaldvogel/209politics/projects/911calls/data/stockton.csv', 'r')
+f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/data/stockton.csv', 'r')
 r = csv.reader(f)
 next(r)
 s = ''
@@ -7,9 +7,9 @@ for i in r:
     s += """L.marker([%s]).bindPopup("<p class='popuptitle'>%s</p><p class='info'>%s at %s - %s</p><p class='details' onclick='show(event);'>Details</p><p class='desc' style='display: none;'>%s</p><p class='hide' style='display: none;' onclick='hide(event);'>Hide</p>").addTo(markers);\n"""%(i[4], i[0], i[1], i[2].lower(), i[3], i[5])
 f.close()
 l = "var markers = L.layerGroup();"
-f = open('/home/miriamwaldvogel/209politics/projects/911calls/stockton.html', 'r')
+f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/stockton.html', 'r')
 g = f.read().split(l, 1)
 f.close()
-f = open('/home/miriamwaldvogel/209politics/projects/911calls/stockton.html', 'w')
-f.write(g[0]+l+"\n"+s+g[1].split("var map", 1)[1]+"var map")
+f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/stockton.html', 'w')
+f.write(g[0]+l+"\n"+s+"var map"+g[1].split("var map", 1)[1])
 f.close()
