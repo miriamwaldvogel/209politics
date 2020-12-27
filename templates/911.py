d7a2e807cc4c7b1703f7f8bc1b6abe5706036657
miriamwaldvogel <miriamwaldvogel@penguin>
2020-12-27 10:45:43 -0800

    geojson

--- templates/911.py
+++ templates/911.py
@@ -3,15 +3,28 @@ f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/data/stockton.csv
 r = csv.reader(f)
 next(r)
 s = ''
-dates = []
+filter = []
 for i in r:
-    s += """        L.marker([%s]).bindPopup("<p class='popuptitle'>%s</p><p class='info'>%s at %s - %s</p><p class='details' onclick='show(event);'>Details</p><p class='desc' style='display: none;'>%s</p><p class='hide' style='display: none;' onclick='hide(event);'>Hide</p>").addTo(markers);\n"""%(i[4], i[0], i[1], i[2].lower(), i[3], i[5])
-    dates.append(i[1])
+    if " " in str(i[4]):
+        a = str(i[4]).split(", ")
+    else:
+        a = str(i[4]).split(",")
+    filter.append({
+      "type": "Feature",
+      "properties": {
+          "type": i[0],
+          "date": i[1],
+          "time": i[2].lower(),
+          "location": i[3],
+          "desc": i[5]
+      },
+      "geometry": {
+          "type": "Point",
+          "coordinates": [float(a[1]), float(a[0])]
+      }
+    })
 f.close()
-l = "var markers = L.layerGroup();"
-f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/stockton.html', 'r')
-g = f.read().split(l, 1)
-f.close()
-f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/stockton.html', 'w')
-f.write(g[0]+l+"\n"+s+"         var dates = %s\n"%dates+"       var map"+g[1].split("var map", 1)[1])
+l = '<script type="text/javascript">'
+f = open('/home/miriamwaldvogel/209politics/projects/crimemaps/data/stockton.js', 'w')
+f.write("var markers = %s;"%filter)
 f.close()
