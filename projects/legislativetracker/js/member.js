c3a228673084ad4e6fa4d1d2d7d160a36605bc11
miriamwaldvogel <miriamwaldvogel@penguin>
2021-01-07 20:46:43 -0800

    full members

--- projects/legislativetracker/js/member.js
+++ projects/legislativetracker/js/member.js
@@ -1,4 +1,4 @@
-var para = {'mcnerney': {'status': {'Introduced': [0, 1, 2]}, 'role': {'sponsor': [], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1], 'Resolution': [2]}, 'date': ['1/4/2021', '1/4/2021', '1/4/2021'], 'area': {'Not assigned': [0, 1, 2]}}};
+var para = {'flora': {'status': {'Introduced': [0]}, 'role': {'sponsor': [], 'cosponsor': [0]}, 'type': {'Bill': [0]}, 'date': ['12/10/2020'], 'area': {'COVID-19': [0]}}, 'cooper': {'status': {'Introduced': [0, 1, 2, 3, 4]}, 'role': {'sponsor': [0, 1, 2, 3], 'cosponsor': [4]}, 'type': {'Bill': [0, 1, 2, 3, 4]}, 'date': ['12/16/2020', '12/7/2020', '12/7/2020', '12/7/2020', '12/7/2020'], 'area': {'Cannabis': [0], 'Housing': [4], 'Peace officers': [2], 'Bail': [3], 'State bodies': [1]}}, 'harder': {'status': {'House Veto Override': [4], 'Failed Senate Committee': [2], 'Introduced': [0], 'Resolving Differences': [3], 'Failed House Committee': [1]}, 'role': {'sponsor': [3, 4], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1, 2, 3, 4]}, 'date': ['12/16/2020', '12/3/2020', '11/24/2020', '8/14/2020', '8/14/2020'], 'area': {'Education': [2], 'Not assigned': [0, 1], 'Water Resources Development': [3, 4]}}, 'villapudua': {'status': {}, 'role': {'sponsor': [], 'cosponsor': []}, 'type': {}, 'date': [], 'area': {}}, 'mcnerney': {'status': {'Introduced': [0, 1, 2]}, 'role': {'sponsor': [], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1], 'Resolution': [2]}, 'date': ['1/4/2021', '1/4/2021', '1/4/2021'], 'area': {'Not assigned': [0, 1, 2]}}, 'eggman': {'status': {'Introduced': [0, 1, 2]}, 'role': {'sponsor': [], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1, 2]}, 'date': ['12/16/2020', '12/7/2020', '12/7/2020'], 'area': {'Communications': [0], 'Housing': [2], 'Drugs/alcohol': [1]}}};
 window.onscroll = function(){
   if (window.innerWidth <= 700){
     if (window.pageYOffset > 10){
@@ -37,6 +37,8 @@ var l = document.getElementsByClassName("leg");
 function filter(pol, check, category){
   var a = document.getElementById(check.replaceAll(" ", "-"));
   var b = para[pol][category][check];
+  var s = Date.parse(document.getElementById("startdate").value);
+  var e = Date.parse(document.getElementById("enddate").value);
   if (a.checked){
     for (var i = 0; i < b.length; i++){
       l[b[i]].classList.remove("hide");
@@ -47,11 +49,12 @@ function filter(pol, check, category){
     }
   }
 }
-function datefilter(se){
+function datefilter(p, se){
   if (se === 'start'){
     var d = Date.parse(document.getElementById("startdate").value);
     for (var i = 0; i < l.length; i++){
-      if (Date.parse(para['date'][i]) < d){
+      document.getElementById("title").innerHTML += " yes";
+      if (Date.parse(para[p]['date'][i]) < d){
         l[i].classList.add("hide");
       } else{
         l[i].classList.remove("hide");
@@ -60,7 +63,7 @@ function datefilter(se){
   } else{
     var d = Date.parse(document.getElementById("enddate").value);
     for (var i = 0; i < l.length; i++){
-      if (Date.parse(para['date'][i]) > d){
+      if (Date.parse(para[p]['date'][i]) > d){
         l[i].classList.add("hide");
       } else{
         l[i].classList.remove("hide");
