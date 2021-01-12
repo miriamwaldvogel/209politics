b90984220b2e5160792f5014c47fe216f465a781
miriamwaldvogel <miriamwaldvogel@penguin>
2021-01-11 19:58:13 -0800

    js modifications

--- projects/legislativetracker/js/member.js
+++ projects/legislativetracker/js/member.js
@@ -1,3 +1,9 @@
+if (window.innerWidth > 899){
+  var a = (document.getElementById("textinfo").offsetHeight - document.getElementById("introinfo").offsetHeight - 50);
+  if (a > 250){
+    document.getElementById("map").style.height = a+'px';
+  }
+}
 var para = {'flora': {'status': {'Introduced': [0]}, 'role': {'sponsor': [], 'cosponsor': [0]}, 'type': {'Bill': [0]}, 'date': ['12/10/2020'], 'area': {'COVID-19': [0]}}, 'cooper': {'status': {'Introduced': [0, 1, 2, 3, 4]}, 'role': {'sponsor': [0, 1, 2, 3], 'cosponsor': [4]}, 'type': {'Bill': [0, 1, 2, 3, 4]}, 'date': ['12/16/2020', '12/7/2020', '12/7/2020', '12/7/2020', '12/7/2020'], 'area': {'Cannabis': [0], 'Housing': [4], 'Peace officers': [2], 'Bail': [3], 'State bodies': [1]}}, 'harder': {'status': {'House Veto Override': [4], 'Failed Senate Committee': [2], 'Introduced': [0], 'Resolving Differences': [3], 'Failed House Committee': [1]}, 'role': {'sponsor': [3, 4], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1, 2, 3, 4]}, 'date': ['12/16/2020', '12/3/2020', '11/24/2020', '8/14/2020', '8/14/2020'], 'area': {'Education': [2], 'Not assigned': [0, 1], 'Water Resources Development': [3, 4]}}, 'villapudua': {'status': {}, 'role': {'sponsor': [], 'cosponsor': []}, 'type': {}, 'date': [], 'area': {}}, 'mcnerney': {'status': {'Introduced': [0, 1, 2]}, 'role': {'sponsor': [], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1], 'Resolution': [2]}, 'date': ['1/4/2021', '1/4/2021', '1/4/2021'], 'area': {'Not assigned': [0, 1, 2]}}, 'eggman': {'status': {'Introduced': [0, 1, 2]}, 'role': {'sponsor': [], 'cosponsor': [0, 1, 2]}, 'type': {'Bill': [0, 1, 2]}, 'date': ['12/16/2020', '12/7/2020', '12/7/2020'], 'area': {'Communications': [0], 'Housing': [2], 'Drugs/alcohol': [1]}}};
 window.onscroll = function(){
   if (window.innerWidth <= 700){
