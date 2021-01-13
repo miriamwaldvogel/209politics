ca6a2a2d5035653cf26cf2e28f9b13d544d91466
miriamwaldvogel <miriamwaldvogel@penguin>
2021-01-13 13:32:38 -0800

    subpage

--- templates/legislation.py
+++ templates/legislation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import csv
+import csv, re
 filter = {}
 sl = 13
 tl = 22
@@ -77,9 +77,8 @@ def leg(pol):
         if i[0] not in allleg:
             allleg[i[0]] = i
         elif i != allleg[i[0]]:
-            #print(i)
-            #print(allleg[i[0]])
-            pass
+            print(i)
+            print(allleg[i[0]])
         filter[pol]['date'].append(i[4])
         if people[pol][1] in i[5]:
             filter[pol]['role']['sponsor'].append(j)
@@ -131,6 +130,182 @@ def leg(pol):
                 %s
             </div>
         </div>""" % (people[pol][2], i[0].replace('.', '', 2).lower().replace(' ', '-', 1), people[pol][0], i[1], i[0], i[2], i[3], i[4], i[5], a, i[7], i[8], s)
+        if i[0] == "A.B. 109":
+            legfile = open("/home/miriamwaldvogel/209politics/projects/legislativetracker/data/legislature/ab-109.csv", "r")
+            legr = csv.reader(legfile)
+            legr.next()
+            involved = legr.next()
+            role = ["Principal coauthors: ", "Coauthors: "]
+            title = ["Asms. ", "Sens. "]
+            z = re.split(", | and |and ", involved[2])
+            allinvolved = ''
+            q = "<p>Introduced by "
+            if "Assembly" in z[0]:
+                if len(z) > 1:
+                    q += "Asms. "
+                else:
+                    q += "Asm. "
+                z[0] = z[0].split(" ", 2)[2]
+            else:
+                if len(z) > 1:
+                    q += "Sens. "
+                else:
+                    q += "Sen. "
+            for k in z:
+                if k != "":
+                    memberinfo = statelegs[0][k]
+                    q += "%s%s (%s-%s), "%(memberinfo[2], k, memberinfo[0], memberinfo[1])
+            q = q[:len(q)-2]
+            q += "</p>\n"
+            allinvolved += q
+            for m, k in enumerate(role):
+                    q = "<p>%s"%k
+                    for o in range(3, 5):
+                        if involved[2*m+o] != "":
+                            q += title[o-3]
+                            for l in re.split(", | and |and ", involved[2*m+o]):
+                                if l != "":
+                                    memberinfo = statelegs[o-3][l]
+                                    q += "%s%s (%s-%s), "%(memberinfo[2], l, memberinfo[0], memberinfo[1])
+                    q = q[:len(q)-2]
+                    q += "</p>\n"
+                    if len(q) > 27:
+                        allinvolved += q
+            actionlist = []
+            for k in legr:
+                actionlist.append('<p class="action">%s - %s</p>'%(k[0], k[1]))
+                lastdate = k[0]
+                lastaction = k[1]
+            actions = ''
+            for k in actionlist[::-1]:
+                actions += k
+            legfile.close()
+            g = open("/home/miriamwaldvogel/209politics/projects/legislativetracker/legislature/ab-109.html", "w")
+            g.write("""<!DOCTYPE html>
+            <html lang="en" dir="ltr">
+            <head>
+              <meta charset="utf-8">
+              <meta name="description" content="">
+              <meta name="keywords" content="">
+              <meta name='viewport' content='width=device-width, initial-scale=1'>
+              <link rel="icon" type="image/png" href="/images/favicon.png">
+              <link rel="stylesheet" href="/css/header.css">
+              <link rel="stylesheet" href="/css/global.css">
+              <link rel="stylesheet" href="/css/footer.css">
+              <link rel="stylesheet" href="../css/legislation.css">
+              <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'>
+              <link href="https://fonts.googleapis.com/css?family=Libre Baskerville" rel="stylesheet">
+              <script type="text/javascript" src="/js/header.js"></script>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-174685284-1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+                gtag('js', new Date());
+                gtag('config', 'UA-174685284-1');
+              </script>
+              <title>%s - 209 Politics Legislative Tracker</title>
+            </head>
+            <body>
+              <div id="content">
+                <div id="headercontainer">
+                  <button type="button" id="sidebar" onclick="sidebar();">&#9776;</button>
+                  <div id="logocontainer">
+                    <a href="https://209politics.com/">
+                      <img src="/images/favicon-horizontal.png" alt="209 Politics" id="logo">
+                    </a>
+                  </div>
+                  <div id="searchcontainer">
+                    <div id="searchimgcontainer" onclick="searchtoggle();">
+                      <img src="/images/search.svg" alt="Search sybmol" id="searchimg">
+                    </div>
+                    <div id="formcontainer1">
+                      <form id="searchform" action="https://www.google.com/search" method="get">
+                        <input type="hidden" name="q" value="site:209politics.com">
+                        <input type="text" name="q" value="" id="searchbox1">
+                        <input type="submit" value="GO" class="searchbutton" id="searchbutton1">
+                      </form>
+                    </div>
+                  </div>
+                </div>
+                <div id="sectionscontainer" class="sections">
+                  <div id="homenavcontainer" class="headernavcontainer">
+                    <a href="https://209politics.com/" id="latest" class="section">HOME</a>
+                  </div>
+                  <div id="latestnavcontainer" class="headernavcontainer">
+                    <a href="/latest" id="latest" class="section">LATEST</a>
+                  </div>
+                  <div id="electionsnavcontainer" class="headernavcontainer">
+                    <a href="/elections" id="elections" class="section">ELECTIONS</a>
+                  </div>
+                  <div id="policynavcontainer" class="headernavcontainer">
+                    <a href="/policy" class="section">POLICY</a>
+                  </div>
+                  <div id="covidnavcontainer" class="headernavcontainer">
+                    <a href="/covid-19" class="section">COVID-19</a>
+                  </div>
+                  <div id="intnavcontainer" class="headernavcontainer">
+                    <a href="/interactives" class="section">INTERACTIVES</a>
+                  </div>
+                </div>
+                <div id="formcontainer2">
+                  <form id="searchform" action="https://www.google.com/search" method="get">
+                    <input type="hidden" name="q" value="site:209politics.com">
+                    <input type="text" name="q" value="" id="searchbox2">
+                    <input type="submit" value="GO" class="searchbutton" id="searchbutton1">
+                  </form>
+                </div>
+                <div id="filler"></div>
+                <div id="titlecontainer">
+                  <div id="backbutton"><a href="" id="backurl">BACK</a></div>
+                  <h1 id="title">%s</h1>
+                  <p id="legtext"><b>%s</b> - %s</p>
+                </div>
+                <div id="firstleginfo">
+                  <p class="are">Policy Area: %s</p>
+                  <p class="date">Introduced: %s</p>
+                  <div id="sponsors">
+                    %s
+                  </div>
+                  <p id="togglesponsors" onclick="sponsortoggle();"></p>
+                  <p class="com">Committees: %s</p>
+                  <p class="actiontop">Latest action: %s - %s</a>
+                  </p>
+                  <div class="statuscontainer">
+                    <p class="statustitle">Status: </p>
+                    %s
+                  </div>
+                </div>
+                <div id="legdesc">
+                  <p class="legdesc"></p>
+                </div>
+                <div id="actions">
+                  <p id="actionstitle">Actions</p>
+                  %s
+                </div>
+                <div id="footer">
+                  <p id="socialmediafooter">
+                    <a href="https://twitter.com/209Politics" id="twitterfooter" class="sociallinkfooter">Twitter: @209Politics</a> •
+                    <a href="https://www.instagram.com/209.politics/" id="instagramfooter" class="sociallinkfooter">Instagram: @209.politics</a> •
+                    <a href="https://www.facebook.com/209politics" id="facebookfooter" class="sociallinkfooter">Facebook: @209Politics</a>
+                  </p>
+                  <p id="navfooter">
+                    <a href="https://209politics.com/" id="homenavfooter" class="navfooter">Home</a> •
+                    <a href="/latest" id="latestnavfooter" class="navfooter">Latest</a> •
+                    <a href="/elections" id="electionsnavfooter" class="navfooter">Elections</a> •
+                    <a href="/policy" id="policynavfooter" class="navfooter">Policy</a> •
+                    <a href="/covid-19" id="covidnavfooter" class="navfooter">COVID-19</a> •
+                    <a href="/interactives" id="covidnavfooter" class="navfooter">Interactives</a> •
+                    <a href="/contact" id="contactnavfooter" class="navfooter">Contact us</a>
+                  </p>
+                </div>
+              </div>
+              <script type="text/javascript" src="../js/leg.js"></script>
+            </body>
+            </html>"""%(i[0], i[0], i[1], i[2], i[3], i[4], allinvolved, i[7], lastdate, lastaction, s, actions))
+            g.close()
     f.close()
     statusfilters = ''
     for i in filter[pol]['status']:
