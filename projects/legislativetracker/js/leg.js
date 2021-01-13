1364d564ee49dbc0cfc555d988c007be9fadc3c6
miriamwaldvogel <miriamwaldvogel@penguin>
2021-01-13 13:57:25 -0800

    sponsor toggle

--- projects/legislativetracker/js/leg.js
+++ projects/legislativetracker/js/leg.js
@@ -1,1 +1,16 @@
 document.getElementById("backurl").href = "/projects/legislativetracker/people/"+new URLSearchParams(window.location.search).get('acq')+".html";
+if (document.getElementById("sponsors").offsetHeight > 31){
+  document.getElementById("togglesponsors").innerHTML = "Show all";
+  document.getElementById("sponsors").style.height = "1.4em";
+}
+function sponsortoggle(){
+  var a = document.getElementById("sponsors");
+  var b = document.getElementById("togglesponsors");
+  if (a.style.height === "1.4em"){
+    a.style.height = null;
+    b.innerHTML = "Hide all";
+  } else{
+    a.style.height = "1.4em";
+    b.innerHTML = "Show all";
+  }
+}
