8001771f36276e9923672062caa50da7c2a27b82
miriamwaldvogel <miriamwaldvogel@penguin>
2021-01-13 07:54:24 -0800

    com filter

--- templates/legislation.py
+++ templates/legislation.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import csv
 filter = {}
 sl = 13
@@ -9,28 +10,48 @@ people = {'mcnerney': ['jerry-mcnerney', 'McNerney', 'congress'],
 'villapudua': ['carlos-villapudua', 'Villapudua', 'legislature'],
 'flora': ['heath-flora', 'Flora', 'legislature']
 }
-stateleg = {"Aguiar-Curry": ["D", "Winters", "Cecilia"],
-"Bloom": ["D", "Santa Monica", "Richard"],
-"Bonta": ["D", "Oakland", "Rob"],
-"Borgeas": ["R", "Fresno", "Andreas"],
-"Caballero": ["D", "Merced", "Anna"],
-"Cooper": ["D", "Elk Grove", "Jim"],
+statelegs = [{"Aguiar-Curry": ["D", "Winters", "Cecilia "],
+"Bauer-Kahan": ["D", "Orinda", "Rebecca "],
+"Bloom": ["D", "Santa Monica", "Richard "],
+"Bonta": ["D", "Oakland", "Rob "],
+"Burke":["D", "Inglewood", "Autumn "],
+"Carrillo":["D", "Los Angeles", "Wendy "],
+"Cooper": ["D", "Elk Grove", "Jim "],
 "Cristina Garcia": ["D", "Bell Gardens", ""],
 "Eduardo Garcia": ["D", "Coachella", ""],
-"Eggman": ["D", "Stockton", "Susan Talamantes"],
-"Flora": ["R", "Manteca", "Heath"],
-"Low": ["D", "Silicon Valley", "Evan"],
-"Petrie-Norris": ["D", "Laguna Beach", "Cottie"],
-"Quirk": ["D", "Hayward", "Bill"],
-"Quirk-Silva": ["D", "Fullerton", "Sharon"],
-"Reyes": ["D", "San Bernardino", "Eloise"],
+"Flora": ["R", "Manteca", "Heath "],
+"Irwin":["D", "Thousand Oaks", "Jacqui "],
+"Low": ["D", "Silicon Valley", "Evan "],
+"Muratsuchi": ["D", "Torrance", "Al "],
+"Petrie-Norris": ["D", "Laguna Beach", "Cottie "],
+"Quirk": ["D", "Hayward", "Bill "],
+"Quirk-Silva": ["D", "Fullerton", "Sharon "],
+"Reyes": ["D", "San Bernardino", "Eloise "],
 "Robert Rivas": ["D", "Hollister", ""],
-"Santiago": ["D", "Los Angeles", "Miguel"],
-"Stone": ["D", "Monterey", "Mark"],
-"Weber": ["D", "San Diego", "Shirley"],
-"Villapudua": ["D", "Stockton", "Carlos"],
-"Wiener": ["D", "San Francisco", "Scott"],
-}
+"Blanca Rubio": ["D", "Baldwin Park", ""],
+"Santiago": ["D", "Los Angeles", "Miguel "],
+"Stone": ["D", "Monterey", "Mark "],
+"Villapudua": ["D", "Stockton", "Carlos "],
+"Wicks": ["D", "Berkeley", "Buffy "],
+"Weber": ["D", "San Diego", "Shirley "],
+"Wood": ["D", "Santa Rosa", "Jim "]
+},
+{
+"Borgeas": ["R", "Fresno", "Andreas "],
+"Caballero": ["D", "Merced", "Anna "],
+"Eggman": ["D", "Stockton", "Susan Talamantes "],
+"Gonzalez":["D", "Long Beach", "Lena "],
+"Limón":["D", "Santa Barbara", "Monique "],
+"McGuire": ["D", "Santa Rosa", "Mike "],
+"Wiener": ["D", "San Francisco", "Scott "],
+}]
+allleg = {}
+def filteradd(p, k, a, j):
+    global filter
+    if a not in filter[p][k]:
+        filter[p][k][a] = [j]
+    else:
+        filter[p][k][a].append(j)
 def makefilters(p, a, n):
     s = """<div class="filters">
 <p class="filtertitle">%s</p>"""%n
@@ -42,30 +63,32 @@ def makefilters(p, a, n):
     s += '\n</div>\n'
     return(s)
 def leg(pol):
-    global filter
+    global filter, allleg
     f = open('/home/miriamwaldvogel/209politics/projects/legislativetracker/data/%s-leg.csv'%pol, 'r')
     filter[pol] = {'type': {}, 'role':{
     'sponsor': [],
     'cosponsor': [],
-    }, 'status':{}, 'area':{}, 'date':[]}
+    }, 'status':{}, 'area':{}, 'committees':{}, 'date':[]}
     r = csv.reader(f)
     header = next(r)
     o = 9
     p = ''
     for j, i in enumerate(r):
+        if i[0] not in allleg:
+            allleg[i[0]] = i
+        elif i != allleg[i[0]]:
+            #print(i)
+            #print(allleg[i[0]])
+            pass
         filter[pol]['date'].append(i[4])
-        if i[3] not in filter[pol]['area']:
-            filter[pol]['area'][i[3]] = [j]
-        else:
-            filter[pol]['area'][i[3]].append(j)
         if people[pol][1] in i[5]:
             filter[pol]['role']['sponsor'].append(j)
         else:
             filter[pol]['role']['cosponsor'].append(j)
-        if i[1] not in filter[pol]['type']:
-            filter[pol]['type'][i[1]] = [j]
-        else:
-            filter[pol]['type'][i[1]].append(j)
+        filteradd(pol, 'area', i[3], j)
+        filteradd(pol, 'type', i[1], j)
+        for k in i[7].split("/"):
+            filteradd(pol, 'committees', k, j)
         s = ''
         legstatus = i.index('TRUE')
         if header[legstatus] in filter[pol]['status']:
@@ -125,7 +148,7 @@ def leg(pol):
     g = f.read().split('<!--Python marker-->', 1)
     f.close()
     f = open('/home/miriamwaldvogel/209politics/projects/legislativetracker/people/%s.html' % people[pol][0], 'w')
-    f.write(g[0]+'<!--Python marker-->\n'+makefilters(pol, 'status', 'Status')+makefilters(pol, 'area', 'Policy area')+makefilters(pol, 'type', 'Legislation type'))
+    f.write(g[0]+'<!--Python marker-->\n'+makefilters(pol, 'type', 'Legislation type')+makefilters(pol, 'status', 'Status')+makefilters(pol, 'area', 'Policy area')+makefilters(pol, 'committees', "Committees"))
     f.write("""<input type="reset" id="datereset" value="Reset all" onclick="showall();">
         </form>
         </div>
