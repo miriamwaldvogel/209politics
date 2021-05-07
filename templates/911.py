6c94a1fd9decd4997e85f85339ffa7c034f5ee7d
miriamwaldvogel <miriamwaldvogel@penguin>
2021-05-06 19:35:12 -0700

    events

--- templates/911.py
+++ templates/911.py
@@ -17,7 +17,7 @@ types = {
 "Officer shooting": ["Officer shooting"],
 "Kidnapping": ["Kidnapping", "Kidnapping arrest"],
 "Mental health call": ["Mental health call"],
-"Other": ["Attempted murder arrest", "Attempted murder", "Suspicious package", "Laser strike arrest", "Criminal threats arrest", "Explosive materials arrest", "Warrant service"],
+"Other": ["Attempted murder arrest", "Attempted murder", "Suspicious package", "Laser strike arrest", "Criminal threats arrest", "Explosive materials arrest", "Warrant service", "Attempted homicide arrest"],
 "Vandalism": ["Vandalism arrest", "Vandalism"],
 "Train": ["Train", "Train vs. pedestrian"],
 "Drowning": ["Drowning"]
