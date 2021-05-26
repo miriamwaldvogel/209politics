91f1ad6c9cdc7db631f0b75a93d570bc4137a948
miriamwaldvogel <miriamwaldvogel@penguin>
2021-05-25 20:39:42 -0700

    large image card

--- templates/articlegenerator.py
+++ templates/articlegenerator.py
@@ -111,7 +111,7 @@ def article():
       <meta name='viewport' content='width=device-width, initial-scale=1'>
       <meta property="og:image" content="https://209politics.com/%s">
       <meta property="og:type" content="article"/>
-      <meta name="twitter:card" content="summary">
+      <meta name="twitter:card" content="summary_large_image">
       <meta name="twitter:site" content="@209politics">
       <meta name="twitter:title" content="%s">
       <meta name="twitter:image" content="https://209politics.com/%s">
