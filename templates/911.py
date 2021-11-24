c21c099a70bb35063a9393a1dcc6c2a0f8059556
miriamwaldvogel <miriamwaldvogel@penguin>
2021-11-23 23:04:22 -0800

    events

--- templates/911.py
+++ templates/911.py
@@ -9,7 +9,7 @@ types = {
 "Vehicle/traffic": ["Traffic fatality", "Stolen vehicle arrest", "Vehicle pursuit arrest", "Traffic pursuit", "Pursuit arrest", "Carjacking", "Hit and run", "Vehicle pursuit", "Traffic injury", "Auto theft arrest", "Carjacking arrest", "Hit and run fatality", "DUI arrest", "Hit and run fatality", "Attempted auto burglary", "Major injury collision"],
 "Homicide": ["Homicide"],
 "Theft/robbery": ["Robbery", "Attempted robbery", "Strong-arm robbery", "Armed robbery", "Auto burglaries", "Robbery arrest"],
-"Residential": ["Residential robbery", "Home invasion", "Burglary", "Home invasion robbery", "Burglary arrest"],
+"Residential": ["Residential robbery", "Home invasion", "Burglary", "Home invasion robbery", "Burglary arrest", "Attempted armed robbery"],
 "Assault": ["Person stabbed", "Assault with a deadly weapon", "Felony assault"],
 "Arrest": ["Resisting arrest", "Battery on an officer", "Assault on an officer", "Disturbance", "Warrant arrest", "Assault"],
 "Fire": ["Arson", "Arson arrest", "Warehouse fire", "Attempted arson"],
